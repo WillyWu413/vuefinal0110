0b1d75e01ae55c0e9173faf6b93644f7a50e598f
吳御廷 <a0931349556@gmail.com>
2020-01-10 14:47:32 +0800

    上傳全部檔案

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    
+    <!-- <HelloWorld/> -->
+    <router-view></router-view>
+    
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/manager/HelloWorld'
+
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import "./assets/all";
+</style>
---
+++ src/components/customer/store.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="customerCart">
+        <div class="container-fluid">
+        <navbar/>
+        <Productheader/>
+        <div class="container">
+              <router-view/>
+        </div>
+        <footerAll/>
+        </div>
+    </div>
+</template>
+
+<script>
+import navbar from './pages/navbar';
+import footerAll from './pages/footerAll'
+import Productheader from './pages/Productheader';
+import sidebar from './pages/sidebar';
+import product from './pages/product';
+import $ from 'jquery';
+
+
+export default {
+    components:{
+        navbar,
+        footerAll,
+        Productheader,
+        sidebar,
+        product,
+    },
+    
+};
+
+</script>
+
+<style scoped>
+    .customerCart{
+        background-color: #000;
+    }
+
+</style>
